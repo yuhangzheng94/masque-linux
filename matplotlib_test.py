a42c690d579311f1d9ac6e4301f4ba414ba2cd8e
yuhangzheng94 <61158372+yuhangzheng94@users.noreply.github.com>
2023-11-26 23:32:51 +0800

    test2 result visualization

---
+++ matplotlib_test.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import csv
+
+# Lists to store the index and time collapse values
+index_values = []
+time_collapse_values = []
+
+# Read the CSV file and extract the data
+with open('test/test2.txt', 'r') as file:
+    csv_reader = csv.reader(file)
+    for row in csv_reader:
+        index_values.append(int(row[0]))
+        time_collapse_values.append(int(row[3]))
+
+# Create the line chart
+plt.plot(index_values, time_collapse_values)
+
+# Set the labels and title
+plt.xlabel('Index')
+plt.ylabel('Time Collapse')
+plt.title('Line Chart')
+
+# Display the chart
+plt.show()
