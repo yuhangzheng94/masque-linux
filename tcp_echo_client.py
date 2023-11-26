31bd36c9ca09cf67437d9aecbaa441fdcaabd871
yuhangzheng94 <61158372+yuhangzheng94@users.noreply.github.com>
2023-11-26 22:51:15 +0800

    test1: gettime, test2: gettime_ns

--- tcp_echo_client.py
+++ tcp_echo_client.py
@@ -14,7 +14,10 @@ LOG_LEVEL = 'debug'
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 all_threads = []
 
+myFile = open("test3.txt", "a")
+
 def test_echo_client(server_ip, client_ip):
+
     # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((client_ip, MASQUE_CLIENT_PORT))
     def send(data):
@@ -24,15 +27,26 @@ def test_echo_client(server_ip, client_ip):
     CONNECT /something/127.0.0.1/12345/ HTTP/1.1
     Host: example.com
     '''
-    send(f'CONNECT {server_ip}:12345/ HTTP/1.1')
-    send(f'Host: {server_ip}:12345')
+    send(f'CONNECT {server_ip}:{ECHO_SERVER_PORT}/ HTTP/1.1')
+    send(f'Host: {server_ip}:{ECHO_SERVER_PORT}')
     time.sleep(0.1)
 
     # 看看有没有得到echo
     for i in range(100):
+        myFile.write(str(i)+",")
+        t_sent = time.clock_gettime_ns(time.CLOCK_REALTIME)
         send(f'Hello world! #{i}')
+        myFile.write(str(t_sent)+",")
+
         data = sock.recv(1024)
+        t_received = time.clock_gettime_ns(time.CLOCK_REALTIME)
+        myFile.write(str(t_received)+",")
         print('TCP client received: ', data)
+
+        diff = t_received - t_sent
+        myFile.write(str(diff)+"\n")
+
+    myFile.close()
     sock.close()
 
 def main():
@@ -46,6 +60,8 @@ if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
+        myFile.close()
+
         sock.close()
         for t in all_threads:
             t.join()
