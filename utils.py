c0c8983f5ca5da7ea4d965dc4b972a39a8d3b964
Me <2643315319@qq.com>
2023-11-30 17:33:38 +0800

    add experiment.ipynb

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import time
 import os
 import threading
 import subprocess
+import socket
 
 # 为了debug我封装了一些函数...
 
@@ -13,23 +14,38 @@ def path_exists(path):
     return os.path.exists(path)
 
 def exec(command):
-    print('\n\n\nExecuting:', command, sep=' ')
+    # print('\n\n\nExecuting:', command, sep=' ')
     os.system(command)
     time.sleep(1/10)
 
 def start(func):
-    print('\n\n\nstarting:', func.__name__)
+    # print('\n\n\nstarting:', func.__name__)
     thread = threading.Thread(target=func)
     thread.start()
     time.sleep(1/10)
+    return thread
+
+def _get_local_ip():
+    # 获取本机ip
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # 任意创建一个socket，观察操作系统提供的ip
+    s.connect(('google.com', 12346))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
+
+local_ip = _get_local_ip()
 
 # 下载编译好的masquerade，保存在~/masque-linux
-if not path_exists('~/masque-linux'):
-    exec('''
-        cd ~
-        sudo git clone https://github.com/dx2102/masque-linux.git
-    ''')
-    time.sleep(4)
+# if not path_exists('~/masque-linux'):
+#     exec('''
+#         cd ~
+#         rm -rf ~/masque-linux
+#         sudo git clone https://github.com/dx2102/masque-linux
+#         cd ~/masque-linux
+#         sudo chmod a+rwx -R .
+#         ''')
+#     time.sleep(0.5)
 
 def kill_process_on_port(port, wait=0.1):
     try:
@@ -48,31 +64,3 @@ def kill_process_on_port(port, wait=0.1):
         print(f"No process found listening on port {port}")
     finally:
         time.sleep(wait)
-
-'''
-我刚刚读了一下masquerade仓库，修正一下我之前说的：
-server不是standalone，它也是一个proxy，
-client则是提供了http/1.1和socks5的传统协议支持。
-
-实际的运行我们需要4个组件：
-一个自定义UDP server，比如一个简单的nc pingpong就可以, 比如监听localhost:12345，
-一个masquerade server听4433，
-一个masquerade client听8989,目标是4433，
-和一个自定义tcp client,目标是8989。
-
-运行时，masquerade client会handle incoming tcp connection，
-我们需要在这个TCP connection的起始位置发一个HTTP/1.1报文
-method=CONNECT， path=/something/127.0.0.1/12345/, 
-这将使得masquerade client尝试和4433建立HTTP/3 on QUIC的连接,
-并转发这个path, 
-masquerade server会parse path的最后两级, 
-也就是127.0.0.1和12345,并尝试和127.0.0.1:12345建立一个raw UDP连接. 
-这样我们就实现了从自定义tcp client到自定义udp server的代理.
-
-在HTTP/1.1报文结束后,不要关闭TCP连接, 
-那么我们发送的内容都会被masquerade client封装成HTTP/3 Data frame并发给masquerade server, 
-然后进一步转发给UDP Server@localhost:12345, 
-nc pingpong会回应它收到的所有内容, 这些内容会沿着原路返回TCP client, 然后我们在TCP client上应该recv到我们发出的内容. 
-
-至于怎么设计性能指标，我们可以下周再说，但我感觉这周跑通应该是可以的。
-'''
